aa46228dcdb263f0bac98178447e93399c476929
HexHands <119904427+HexHands@users.noreply.github.com>
2023-05-14 18:35:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import time
 start_time = time.time()
 
 while True:
-time.sleep(5)
-print(time.time() - start_time)
-if time.time() - start_time > 120:
-break
+  time.sleep(5)
+  print(time.time() - start_time)
+  if time.time() - start_time > 120:
+    break
 
 print("Script has ended!")
 print(time.time() - start_time)
